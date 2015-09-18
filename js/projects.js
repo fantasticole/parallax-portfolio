274875d41aea18769eae5d1689f3da02856eb330
FantastiCole <coleamurphy@gmail.com>
2015-09-18 01:02:48 -0400

    Add and format Twitterbot timeline, fix panel collapse issues, update navbar mobile styling and corresponding js.

--- js/projects.js
+++ js/projects.js
@@ -5,18 +5,44 @@ $(function(){
         var move = width/20;
         console.log(move);
 
-        $('html, body').animate({
-            scrollLeft: $(loc).offset().left - move
-        }, 600);
+        if ($(window).width > 550){
+	        $('html, body').animate({
+	            scrollLeft: $(loc).offset().left - move
+	        }, 600);
+        }
+        else{
+        	move /=2;
+	        $('html, body').animate({
+	            scrollLeft: $(loc).offset().left - move
+	        }, 600);
+        }
         event.preventDefault();
     });
 
    	$('.panel-title a').click(function(){
+   		var mom = $(this)[0].dataset.parent.toString();
 		$('i', this).toggleClass('down');
-		$('.panel-title a').not(this).each(function(){
+		$(mom + ' .panel-title a').not(this).each(function(){
 			$('i', this).removeClass('down');
 		})
-	})
+	});
+
+	var config = {
+		"id": '628733595385995265',
+		"domId": 'timeline',
+		"maxTweets": 15,
+		"enableLinks": true,
+		"showUser": true,
+		"showTime": true,
+		"showInteraction": false,
+		"lang": 'en'
+	};
+
+	var here = window.location.href;
+
+	if (here.indexOf('projects') > -1){
+		twitterFetcher.fetch(config);
+	};
 });
 
 
